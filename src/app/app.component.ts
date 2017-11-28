714417f43a4fca51327400c0476abff920927815
Lardge <larsbf@gmail.com>
2017-11-28 11:07:35 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, NgModule, EventEmitter }                              from '@angular/core';
+import { Component, NgModule, EventEmitter, ChangeDetectorRef }           from '@angular/core';
 import { MatIcon, MatIconButtonCssMatStyler, MatIconBase, MatIconModule } from '@angular/material';
 import { AnimationEvent, trigger, state, style, group, }                  from '@angular/animations';
 import { animate, transition, keyframes, stagger, query}                  from '@angular/animations';
+import { MediaMatcher }                                                   from '@angular/cdk/layout';
 
 // ROUTER TRANSITIONS
 const routerTransition = trigger('routerTransition', [
@@ -50,13 +51,35 @@ const flyInOutAnimation = trigger('flyInOut', [
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
-  animations: [ flyInOutAnimation, routerTransition]
+  // animations: [ flyInOutAnimation ] // , routerTransition
 })
 
 export class AppComponent {
-  title = 'Portfolio V2';
-  checked = true;
-  disabled = true;
+  // title = 'Portfolio V2';
+
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array(50).fill(0).map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
   getState(outlet) {
     return outlet.activatedRouteData.state;
@@ -70,3 +93,6 @@ export class AppComponent {
     console.warn('Animation done: ', event);
   }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,8 +30,14 @@ import {
   MatSliderModule,
   MatSlideToggleModule,
   MatSnackBarModule,
-  MatSortModule
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
 }                                  from '@angular/material';
+import {CdkTableModule}            from '@angular/cdk/table';
 
 // COMPOENTS AND SERVICES
 import { AppComponent }            from './app.component';
@@ -41,25 +47,18 @@ import { HomeComponent }           from './components/home/home.component';
 import { ProjectsComponent }       from './components/projects/projects.component';
 import { WhyMeComponent }          from './components/why-me/why-me.component';
 
+// MATERIAL MODULE
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutMeComponent,
-    HomeComponent,
-    ProjectsComponent,
-    WhyMeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
+  exports: [
+    CdkTableModule,
     MatAutocompleteModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
     MatCheckboxModule,
     MatChipsModule,
+    MatStepperModule,
     MatDatepickerModule,
     MatDialogModule,
     MatExpansionModule,
@@ -80,35 +79,30 @@ import { WhyMeComponent }          from './components/why-me/why-me.component';
     MatSlideToggleModule,
     MatSnackBarModule,
     MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class MaterialModule {}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    HomeComponent,
+    ProjectsComponent,
+    WhyMeComponent
   ],
-  exports: [
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
