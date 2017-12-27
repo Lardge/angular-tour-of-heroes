d1fa41207a3f3e721aa75d177ab220c1c3a7cc04
Lardge <larsbf@gmail.com>
2017-12-27 11:21:43 +0100

    home screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild }                from '@angular/core';
+import { HostListener, ElementRef }                           from '@angular/core';
 import { ActivatedRoute }                                     from '@angular/router';
 import { Location }                                           from '@angular/common';
 import { AnimationEvent, trigger, state, style, query}        from '@angular/animations';
@@ -39,11 +40,35 @@ const homeAnimation = trigger('homeAnimation', [
   ])
 ]);
 
+const scrollAnimation = trigger('scrollAnimation', [
+  state('show', style({
+    opacity: 1 // ,
+    // transform: 'translateX(0)'
+  })),
+  state('hide',   style({
+    opacity: 0,
+    transform: 'translateX(-100%)'
+  })),
+  transition('show => hide', animate('700ms cubic-bezier(.1,1,.1,1)')),
+  transition('hide => show', animate('700ms cubic-bezier(.1,1,.1,1)'))
+    /*/animate('1s ease-in', keyframes([
+      style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+      style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+      style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+    ]))*/
+  // ])),
+  // transition('hide => show', animate('700ms cubic-bezier(.1,1,.1,1)'))
+  /* query('show', stagger('100ms', [
+    animate('700ms cubic-bezier(.1,1,.1,1)')
+  ])))*/
+  // stagger('100ms', [animate('700ms cubic-bezier(.1,1,.1,1)')]))
+]);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
-  animations: [homeAnimation]
+  animations: [homeAnimation, scrollAnimation],
 })
 
 export class HomeComponent implements OnInit {
@@ -52,18 +77,36 @@ export class HomeComponent implements OnInit {
   homeTitleArray = [];
   homeSubTitleArray = [];
 
-  constructor() { }
+  // ANIMATION VARIBALES
+  scrollAnimationState = 'hide';
+
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
     this.homeTitleArray = this.homeTitle.split('');
     this.homeSubTitleArray = this.homeSubTitle.split('');
   }
 
-  animationStarted(event: AnimationEvent) {
-    console.warn('Animation started: ', event);
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll($event) {
+    const componentPosition = this.el.nativeElement.offsetParent.offsetParent.offsetHeight;
+    const scrollPosition = $event.path[0].scrollTop;
+    // const scrollPosition = window.pageYOffset;
+    const halfWindowHeight = window.innerHeight / 2;
+
+    if (scrollPosition  >= (componentPosition - halfWindowHeight)) {
+      this.scrollAnimationState = 'show';
+    } else {
+      this.scrollAnimationState = 'hide';
+    }
+
   }
 
-  animationDone(event: AnimationEvent) {
-    console.warn('Animation done: ', event);
+ /* @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event) {
+      console.log('scrolling...');
   }
+  */
+
+
 }
